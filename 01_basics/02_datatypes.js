1122ec0a39799bed6f7e36e9cac018b968b54af4
AyaanSingh1 <aaritsingh1036@gmail.com>
2025-06-30 14:48:58 +0000

    code for datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,18 @@
 "use strict"; //treat all JS code as newer version
 // alert(3+3) // we are using nodejs,not browser
+// code readability should be high
+let name="Ayaan"
+let age=18
+let isLoggedIn=false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null =>standalone value
+// undefined => 
+//symbol
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
