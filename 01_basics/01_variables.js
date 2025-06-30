0f5a5cfedfd5bca865d6beedde9abb08c6181a4d
AyaanSingh1 <aaritsingh1036@gmail.com>
2025-06-30 14:33:13 +0000

    code for datatyes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="ayaan@gmail.com"
+var accountPassword="123456"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 // not allowed
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Begaluru"
+console.log(accountId);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
