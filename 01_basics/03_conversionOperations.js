0b45252f7c3e9487d5489c2f04123c2e064dd53b
AyaanSingh1 <aaritsingh1036@gmail.com>
2025-07-04 13:53:41 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+// let score="ayaan"
+// console.log(typeof score);
+// let valueNumber=Number(score)
+// console.log(typeof valueNumber);
+
+// console.log(valueNumber);
+//"33"=>33
+// "33abc"=>NaN
+// ture=>1; false =>0
+
+// let isLoggedIn=""
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanisLoggedIn);
+// ""=>false
+// "hitesh"=>true
+let someNumber=33
+let stringNumber=String(someNumber)
+    console.log(typeof stringNumber);
